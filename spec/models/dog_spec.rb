9bfaad8f6389aec847babebb970c5bd14c99ae1e
Brandon Seroyer <brandonseroyer@gmail.com>
2015-11-03 21:19:52 -0800

    add and pass specs

---
+++ spec/models/dog_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe Dog do
+
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :breed }
+  it { should validate_presence_of :sex }
+  it { should validate_presence_of :age }
+  it { should validate_presence_of :info }
+
+end
