3f95ac4522a75285eafd108cb55f8cf4a8d0b634
Brandon Seroyer <brandonseroyer@gmail.com>
2015-11-03 16:40:17 -0800

    add DogsController

---
+++ app/controllers/dogs_controller.rb
@@ -0,0 +1,56 @@
+class DogsController < ApplicationController
+  before_action :find_dog, except: [:new, :create, :index]
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    @dogs = Dog.all.order(created_at: :desc)
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+    render :show
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    @dog = Dog.new(dog_params)
+    if @dog.save
+      redirect_to  dogs_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @dog = Dog.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @dog = Dog.find(params[:id])
+
+    if @dog.update(dog_params)
+      redirect_to dog_path(@dog)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @dog = Dog.find(params[:id])
+    @dog.destroy
+    redirect_to dogs_path
+  end
+
+  private
+  def dog_params
+    params.require(:dog).permit(:name, :sex, :age, :info :image, :user_id)
+  end
+
+  def find_dog
+    @dog = Dog.find(params[:id])
+  end
+end
