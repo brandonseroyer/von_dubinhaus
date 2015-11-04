75e7c523905299c9d71453bba7e2729c8730e5c0
Brandon Seroyer <brandonseroyer@gmail.com>
2015-11-03 16:19:32 -0800

    add view files for dog class

---
+++ app/models/dog.rb
@@ -0,0 +1,12 @@
+class Dog < ActiveRecord::Base
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :sex, presence: true
+  validates :age, presence: true
+  validates :info, presence: true
+
+  has_attached_file :image, :styles => {:standard => "750x500", :profile => "1250x830", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+end
