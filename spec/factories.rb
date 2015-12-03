843bb6c63fce0a3c6f88173235301a9a414891c1
Brandon Seroyer <brandonseroyer@gmail.com>
2015-12-02 19:58:43 -0800

    add integration spec for delete dog function

---
+++ spec/factories.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+
+  factory(:dog) do
+    name('Sky')
+    breed('German Shephrd Dog')
+    age('5')
+    sex('Female')
+    info('I want this dog')
+  end
+
+end
---
+++ spec/features/delete_dog_pages_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "the delete a post process" do
+  it "delete a post on the app" do
+    login_user
+    post = FactoryGirl.create(:dog)
+    visit dogs_path
+    click_on "Sky"
+    click_on "Delete"
+    expect(page).to have_no_content "Sky"
+  end
+  def login_user
+    visit dogs_path
+    click_link "Sign up"
+    fill_in "Email", with: "dog@dog.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign up"
+  end
+end
