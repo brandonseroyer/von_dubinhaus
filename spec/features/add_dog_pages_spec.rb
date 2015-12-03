a606c80f3a3265655673571d48c740859fb32b5a
Brandon Seroyer <brandonseroyer@gmail.com>
2015-12-02 19:36:08 -0800

    add integration spec for adding a new dog profile

--- spec/features/add_dog_pages_spec.rb
+++ spec/features/add_dog_pages_spec.rb
@@ -2,20 +2,23 @@ require 'rails_helper'
 
 describe "the add a dog process" do
   it "adds a new dog" do
-    visit new_dog_path
+    login_user
+    visit dogs_path
+    click_link "Add Dog"
     fill_in 'Name', :with => 'Sky'
     fill_in 'Breed', :with => 'German Shepherd Dog'
     fill_in 'Age', :with => '6'
     fill_in 'Sex', :with => 'Female'
     fill_in 'Info', :with => 'I want this dog'
-    fill_in 'Image', :with => 'sky.jpg'
     click_on 'Create Dog'
-    expect(page).to have_content 'Dogs'
+    expect(page).to have_content 'Sky'
   end
-
-    it "gives error when no name is entered" do
-    visit new_dog_path
-    click_on 'Create Dog'
-    expect(page).to have_content 'errors'
+  def login_user
+    visit dogs_path
+    click_link "Sign up"
+    fill_in "Email", with: "dog@dog.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign up"
   end
 end
