927c7c93eb368afb02a7abb7334f914d5ec9227d
Brandon Seroyer <brandonseroyer@gmail.com>
2016-03-25 17:25:28 -0700

    re-format and style rescue page

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -6,6 +6,6 @@ class Dog < ActiveRecord::Base
   validates :age, presence: true
   validates :info, presence: true
   validates :breed, presence: true
-  has_attached_file :image, :styles => {:standard => "750x500", :profile => "500x350", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => {:standard => "750x500", :profile => "450x300", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
