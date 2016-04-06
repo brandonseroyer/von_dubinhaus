3919127573777e27238c727325b2eaec18e3e01c
Brandon Seroyer <brandonseroyer@gmail.com>
2016-04-05 17:56:12 -0700

    add carousel to rescue alumni page

--- app/models/testimonial.rb
+++ app/models/testimonial.rb
@@ -1,8 +1,6 @@
 class Testimonial < ActiveRecord::Base
   belongs_to :user
   validates :name, presence: true
-  validates :date, presence: true
-  validates :story, presence: true
   has_attached_file :image, :styles => {:standard => "750x500", :profile => "500x350", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
