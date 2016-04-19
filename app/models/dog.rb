63dd9e16c74d6882dd31fe35703084c233695d38
Brandon Seroyer <brandonseroyer@gmail.com>
2016-04-19 12:07:51 -0700

    add language to point app at correct s3 bucket

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -6,11 +6,11 @@ class Dog < ActiveRecord::Base
   validates :age, presence: true
   validates :info, presence: true
   validates :breed, presence: true
-  has_attached_file :image,
-                    :styles => {:standard => "750x500", :profile => "450x300", :thumb => "300x225" },
-                    :storage => :s3,
-                    :s3_credentials => "#{Rails.root}/config/s3.yml",
-                    :path => "/images/:id/:style.:extension",
-                    :url => ":s3_domain_url"
+  has_attached_file :image, styles: {
+    standard: '750x500',
+    profile: '450x300',
+    thumb: '300x225'
+   }
+   
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
