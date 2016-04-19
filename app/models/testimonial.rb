6856563e517f6aa0819c94eb53ad6419a712fd75
Brandon Seroyer <brandonseroyer@gmail.com>
2016-04-18 18:50:08 -0700

    correct image validation syntax in models

--- app/models/testimonial.rb
+++ app/models/testimonial.rb
@@ -1,11 +1,11 @@
 class Testimonial < ActiveRecord::Base
   belongs_to :user
   validates :name, presence: true
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   has_attached_file :image,
                     :styles => {:standard => "750x500", :profile => "450x300", :thumb => "300x225" },
                     :storage => :s3,
                     :s3_credentials => "#{Rails.root}/config/s3.yml",
                     :path => "/images/:id/:style.:extension",
                     :url => ":s3_domain_url"
-                  end
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+end
