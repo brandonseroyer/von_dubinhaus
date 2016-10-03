a54a7e8fedef57a96e57777567794880318781ed
Brandon Seroyer <brandonseroyer@gmail.com>
2016-10-03 14:38:53 -0700

    update config settings for actionmailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,18 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { :host => "http://www.vondubinhaus.com" }
+  config.action_mailer.delivery_method = :sendmail
+    # Defaults to:
+    # config.action_mailer.sendmail_settings = {
+    #   location: '/usr/sbin/sendmail',
+    #   arguments: '-i -t'
+    # }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'no-reply@vondubinhaus.com'}
+
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
