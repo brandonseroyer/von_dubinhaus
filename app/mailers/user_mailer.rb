6ab4ee3e4a43ef1e2e1d80f63c9c8540dd6e3ade
Brandon Seroyer <brandonseroyer@gmail.com>
2016-10-03 16:20:15 -0700

    undo refactoring that was affecting send

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,14 @@
 class UserMailer < ApplicationMailer
   default from: 'notifications@vondubinhaus.com'
-  @user = 'brandonseroyer@gmail.com'
-  
+
   def application_email
+    @user = 'brandonseroyer@gmail.com'
     mail(to: @user, subject: 'A new Adoption Application has been submited!')
   end
 
   def comment_email(dog)
     @dog = dog
+    @user = 'brandonseroyer@gmail.com'
     mail(to: @user, subject: 'A new Comment has been made on www.vondubinhaus.com')
   end
 end
