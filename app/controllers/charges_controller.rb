d0099f93236ae79c599dc507c6336ce57a7539fe
Brandon Seroyer <brandonseroyer@gmail.com>
2016-11-06 22:07:15 -0800

    send proper amounts to Stripe whether round or float integers

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,6 +1,11 @@
 class ChargesController < ApplicationController
   def create
-    @amount = Donation.find(params[:donation_id]).amount * 100
+    @amount = Donation.find(params[:donation_id]).amount.to_s.split(//)
+    if @amount.last == "0"
+      @subtotal = @amount.join.to_i.round * 100
+    else
+      @subtotal = @amount.join.gsub(".","").to_i
+    end
 
     customer = Stripe::Customer.create(
       email: 'example@stripe.com',
@@ -9,7 +14,7 @@ class ChargesController < ApplicationController
 
     charge = Stripe::Charge.create(
       customer: customer.id,
-      amount: @amount,
+      amount: @subtotal,
       description: 'Rails Stripe customer',
       currency: 'usd'
     )
