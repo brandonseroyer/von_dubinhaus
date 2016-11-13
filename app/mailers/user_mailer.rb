d71f6049a07b6f8a27a93be1bb4d396c08033e8d
Brandon Seroyer <brandonseroyer@gmail.com>
2016-11-13 14:02:09 -0800

    update email address receipient of automated charge email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,24 +2,24 @@ class UserMailer < ApplicationMailer
   default from: 'notifications@vondubinhaus.com'
 
   def application_email
-    @user = 'brandonseroyer@gmail.com'
-    mail(to: @user, subject: 'A new Adoption Application has been submited!')
+    @brandon = 'brandonseroyer@gmail.com'
+    mail(to: @brandon, subject: 'A new Adoption Application has been submited!')
   end
 
   def comment_email(dog)
     @dog = dog
-    @user = 'brandonseroyer@gmail.com'
-    mail(to: @user, subject: 'A new Comment has been made on www.vondubinhaus.com')
+    @brandon = 'brandonseroyer@gmail.com'
+    mail(to: @brandon, subject: 'A new Comment has been made on www.vondubinhaus.com')
   end
 
   def donation_email
-    @user = 'brandonseroyer@gmail.com'
-    mail(to: @user, subject: 'A Donation has been made to Von Dubinhaus Shepherds!')
+    @brandon = 'brandonseroyer@gmail.com'
+    mail(to: @brandon, subject: 'A Donation has been made to Von Dubinhaus Shepherds!')
   end
 
   def charge_email(donation)
     @donation = donation
-    @user = 'brandonseroyer@gmail.com'
-    mail(to: @user, subject: 'A Donation has been made to Von Dubinhaus Shepherds!')
+    @paul = 'vondubinhaus@gmail.com'
+    mail(to: @paul, subject: 'A new Payment has been made to Von Dubinhaus Shepherds!')
   end
 end
